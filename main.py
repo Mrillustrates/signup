a9925031c5b72813cc7d92542fc10371bf2493d2
Chris <lawrenc@live.com>
2016-08-28 23:51:11 -0500

    First commit

--- main.py
+++ main.py
@@ -1,25 +1,102 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 
+sign_in = """
+<html>
+<head>
+<title> Log- In </title>
+</head>
+<div style = "background-color: silver"
+<body>
+<h1><strong> Sign in</strong><h1>
+<form action="/sign-in" method = "post">
+<label> Name:
+ 	<input type = "text" name = "login" value ='%(login)s'>
+    </label>
+    %(login_error)s
+    <br>
+<label> Password:
+    <input type = "password" name = "pword" value = ''>
+    </label>
+    %(pass_error)s
+
+    <br>
+<label> Verify Password:
+    <input type = "password" name = "vpword" value = ''>
+
+
+    </label>
+    <br>
+
+<label> Email(Optional):
+    <input type = "text" name = "mail" value = '%(mail)s'>
+</label>
+ %(email_error)s
+<br>
+<div style = "color : red"> %(error)s </div>
+ <input type="submit" value = "Submit"/>
+ </form>
+ </body>
+ </div>
+ </html>
+ """
+
+
+# create a function to write form with errors with empty placeholders for password and verify pass
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, login_error = "", email_error = "", pass_error = "", login = "", mail ="", error = ""):
+        self.response.out.write(sign_in % { "login_error": login_error,
+                                            "email_error": email_error,
+                                            "pass_error": pass_error,
+                                            "login": login,
+                                            "mail": mail,
+                                            "error": error,
+                                                })
+
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        name = self.request.get('login')
+        password = self.request.get('pword')
+        vpass = self.request.get('vpword')
+        email = self.request.get('mail')
+        s_name = name.split()
+        big_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        small_alpha = "abcdefghijklmnopqrstuvwxyz"  # reference var for small abcs
+        zero = ' '
+        nil = ''
+        e_split = email.split()
+        email_at = '@'
+        email_dot = '.'
+        email_check = email.find(email_at)
+        email_check2 = email.find(email_dot)
+        email_check3 = email.find(zero)
+
+        #if nothing entered
+        if (name == nil and password == nil and email == nil or name == nil and password == nil):
+            self.write_form(nil, nil, nil, nil, nil, "Login information required.")
+        if (name == nil and password != nil and email == nil or name ==nil and password != nil and email != nil):
+            self.write_form("Username required", nil, nil, nil, email, nil)
+        #if space in user name
+        if(len(s_name))  > 1 and name != nil:
+            self.write_form("Username cannot have any spaces", nil, nil, name, email, nil )
+        #if invalid password
+        if(name != nil and password != nil and len(password) < 1 and password != vpass or name != nil and email != nil and password != nil and password != vpass and email_check > 0 and email_check2 > 0 and email_check3 < 0):
+            self.write_form(nil, nil, "Invalid password", name, email, nil)
+        if(name != nil and password == nil):
+            self.write_form(nil,nil, "Password Required", name, email, nil)
+
+
+        if(email != nil and email_check < 1 and email_check2 < 1):
+            self.write_form(nil, "Invalid email", nil, name, email)
+
+        #if successful security check. Login successful!
+        if (len(s_name) == 1 and name != nil and len(password) >= 1 and password == vpass and email != nil and email_check > 0 and email_check2 > 0 and email_check3 < 0 or len(s_name) == 1 and name != nil and len(password) >= 1 and password == vpass and email == nil ):
+            self.response.write("<strong>Welcome</strong> " + name + "!")
+            oops = True
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/sign-in', MainHandler)
 ], debug=True)
